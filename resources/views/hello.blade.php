9e2308b745a3e04e2f91f9c081bc9cd4cbb9dc59
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-12 06:00:23 +0100

    Atualizações de código: rota, autenticação e estilos***

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+
+            <!-- Page Heading -->
+
+
+            <!-- Page Content -->
+            <main>
+                <h1>Hello</h1>
+            </main>
+        </div>
+    </body>
+</html>
